5e7171a21e0ae7affa66878eb70c589a39814ec6
burikunayoni <dabrixpert@gmail.com>
2020-09-06 14:04:39 +0800

    20200906_v0.052

--- cpp_version/idkwtfisthis.cpp
+++ cpp_version/idkwtfisthis.cpp
@@ -10,28 +10,22 @@ void g_Content();
 string samp(int samp);
 int main()
 {
-	title:
-	char s_game; //s_game=start_the_game
-	system("cls");
-	welcomeScr();
-	printf("\n\nHIGHSCORE:%4d", highscore);
-	in:
-	s_game = getch();
-	if(s_game == 32)
+	for(;;)
 	{
+		do 
+		{
+			system("cls");
+			welcomeScr();
+			printf("\n\nHIGHSCORE:%4d", highscore);
+		}while(getch() != 32);
 		g_Content();
-		goto title;
-	}
-	else
-	{
-		goto in;
 	}
 	system("pause");
 	return 0;
 }
 void welcomeScr()//welcome_screen
 {
-	printf("VER:0.051.200905\n\n\n");
+	printf("VER:0.052.200906\n\n\n");
 	printf("PRESS SPACE TO START THE GAME.\n\n\n");
 	printf("MADE BY DABRiXPERT\n");
 	printf("Twitter: @buriku_nayoni\n");
@@ -58,7 +52,6 @@ void g_Content()//game
 //-------------------------------------------------------------------------------------------------
 	system("cls");
 	int in,score=0;
-	char input;
 	int gameover=0;
 	while(gameover==0)
 	{
@@ -69,9 +62,7 @@ void g_Content()//game
 		cout<<line[13];
 		cout<<line[14];
 		printf("\n\nSCORE:%4d",score);
-		none://wrong input(null)
-		input = getch();
-		switch(input)
+		switch(getch())
 		{
 			case 'D':
 			case 'd':
@@ -90,13 +81,13 @@ void g_Content()//game
 				in = 4;
 				break;
 			default:
-				goto none;
+				in = 999;
 		}
-		if(in != l[12])
+		if(in != l[12] && in != 999)
 		{
 			gameover++;
 		}
-		else
+		else if(in == l[12] && in != 999)
 		{
 			for(int i = 12; i >= 1; i--)
 			{
@@ -108,21 +99,23 @@ void g_Content()//game
 			score++;
 			system("cls");
 		}
+		else
+			system("cls");
 	}
-	system("cls");
-	printf("GAME OVER!\n");
-	printf("YOUR SCORE:%4d\n",score);
+	do 
+	{
+		system("cls");
+		printf("GAME OVER!\n");
+		printf("YOUR SCORE:%4d\n",score);
+		if(score>highscore)
+		{
+			printf("\nNEW BEST SCORE!!!\n");
+		}
+		printf("\nPRESS SPACE TO GO BACK TO TITLE SCREEN.");
+	}while(getch() != 32);
 	if(score>highscore)
 	{
 		highscore = score;
-		printf("\nNEW BEST SCORE!!!\n");
-	}
-	printf("\nPRESS SPACE TO GO BACK TO TITLE SCREEN.");
-	end:
-	e_game = getch();
-	if(e_game != 32)
-	{
-		goto end;
 	}
 }
 string samp(int samp)//note_sample 
